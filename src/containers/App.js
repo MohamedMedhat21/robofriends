e39d0e8cc5db5e612aac3a82ecb539329681d161
Mohamed Medhat <mohamedmmedhat21@gmail.com>
2021-08-01 21:07:36 +0200

    adding ico & title & converting app class to hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,42 +1,39 @@
-import React ,{Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 
-class App extends Component{
-    constructor(){
-        super()
-        this.state={
-            robots:[],
-            searchfield:''
-        }
+const App = () => {
+    const [searchField, setsearchField] = useState('');
+    const [robots, setRobots] = useState([]);
+
+    const onSearchChange = (event) => {
+        setsearchField(event.target.value);
     }
-    onSearchChange=(event)=>{
-        this.setState({searchfield:event.target.value});
-    }
-    componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users').then(response=>response.json())
-        .then(users=>{this.setState({robots:users})});
-    }
-    render(){
-        const {robots,searchfield}=this.state;
-        const filteredRobots=robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-        return(
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <Scroll>
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => { setRobots(users) });
+    }, []);
+
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    })
+
+    return (
+        <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChange={onSearchChange} />
+            <Scroll>
                 <ErrorBoundry>
-                    <CardList robots={filteredRobots}/>
-                </ErrorBoundry>                    
-                </Scroll>
-            </div>
-        );
-    }
+                    <CardList robots={filteredRobots} />
+                </ErrorBoundry>
+            </Scroll>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
-ReactDOM.render(
-<App/>
-,document.getElementById('root'));
+ReactDOM.render(<App/>,document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
